c3f647b4bb27721861e7ca7271c0591645dc8d1f
Yazan Boshmaf <yazan.boshmaf@gmail.com>
2016-02-21 23:59:11 -0800

    #8 Minor refactor

--- www/js/blocks/firebase/firebase.factory.datastore.js
+++ www/js/blocks/firebase/firebase.factory.datastore.js
@@ -1,38 +1,37 @@
 (function() {
   'use strict';
 
-  firebaseDataStore.$inject = ['firebaseHandler', 'logger'];
   angular
     .module('blocks.firebase')
     .factory('firebaseDataStore', firebaseDataStore);
 
   /* @ngInject */
-  function firebaseDataStore(firebaseHandler, logger) {
-    var rootRef = firebaseHandler.getRootRef();
+  function firebaseDataStore(firebaseHandler) {
+    var trackingRef = firebaseHandler.getRootRef().child('tracking');
     var userId = null;
 
     var factory = {
-      trackingRef: rootRef.child('tracking'),
-      setUserId: setUserId,
-      log: log
+      log: log,
+      setUserId: setUserId
     };
     return factory;
+
+    function log(message, data) {
+      if (userId) {
+        trackingRef
+          .child(userId)
+          .push({
+            message: message,
+            data: data
+          });
+      }
+    }
 
     function setUserId(id) {
       userId = id;
     }
-
-    function log(message, data) {
-      if (userId) {
-        factory
-        .trackingRef
-        .child(userId)
-        .push({
-          message: message,
-          data: data
-        });
-      }
-    }
   }
+
+  firebaseDataStore.$inject = ['firebaseHandler'];
 
 })();
